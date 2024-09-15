ecd3ab7c2b8ef74d2106e34e62e5028ebe26b348
liam harvell <liam@harvell.us>
2024-09-15 12:21:12 -0400

    Gotten the session user's contact list from the db

---
+++ LAMPAPI/PopulateContactList.php
@@ -0,0 +1,51 @@
+<?php
+
+	$inData = getRequestInfo();
+	//TODO: this connection will have to be updated
+	$conn = new mysqli("localhost", "root", "", "ContactManager");
+	if( $conn->connect_error )
+	{
+		returnWithError( $conn->connect_error );
+	}
+	else
+	{
+		//requests the database for the contacts of the session user and returns it
+		$array = array();
+		$stmt = $conn->prepare("SELECT * FROM Contacts WHERE UserID=?;");
+		$stmt->bind_param("i", $inData['userId']);
+		$stmt->execute();
+		$result = $stmt->get_result();
+
+		while($row = $result->fetch_assoc()) {
+			$array[] = $row;
+		}
+		//This echo is how we have been returning the data so far
+		echo json_encode($array);
+    
+		$stmt->close();
+		$conn->close();
+	}
+
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+
+	function returnWithError( $err )
+	{
+		$retValue = '{"id":0,"FirstName":"","LastName":"","error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+
+	function returnWithInfo( $FirstName, $LastName, $id )
+	{
+		$retValue = '{"id":' . $id . ',"FirstName":"' . $FirstName . '","LastName":"' . $LastName . '","error":""}';
+		sendResultInfoAsJson( $retValue );
+	}
+?>
